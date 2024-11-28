5433de068a52b8556ead0cfecae75ad73d488e1f
Najdorf12 <agustin.morro@gmail.com>
2024-11-28 17:58:21 -0300

    add nodemailer

--- src/app.js
+++ src/app.js
@@ -7,23 +7,24 @@ import authRoutes from "./routes/auth.routes.js";
 import newsRoutes from "./routes/news.routes.js";
 import tournamentsRoutes from "./routes/tournaments.routes.js";
 import gamesRoutes from "./routes/games.routes.js";
-
+import emailRoutes from "./routes/email.routes.js";
 const app = express();
-app.use(cors({/*  http://localhost:5173 https://fausti-oro.vercel.app */
-    origin:"https://fausti-oro.vercel.app",
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://fausti-oro.vercel.app"],
     credentials: true,
-}));
+  })
+);
 
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-app.use("/api/news", newsRoutes );
-app.use("/api/tournaments", tournamentsRoutes );
+app.use("/api/news", newsRoutes);
+app.use("/api/tournaments", tournamentsRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/games", gamesRoutes);
-
-
+app.use("/api/send-email", emailRoutes);
 
 export default app;
